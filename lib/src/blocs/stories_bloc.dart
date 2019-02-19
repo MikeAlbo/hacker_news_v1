a915907e7188c89c7beb875ff9b82bc250043e95
Michael Albonetti <malbo24@gmail.com>
2019-02-19 12:03:39 -0700

    add todos for stories bloc

---
+++ lib/src/blocs/stories_bloc.dart
@@ -0,0 +1,10 @@
+// todo: import models and repository
+// todo: init the repository
+// todo: create PublishSubjects (rxdart) for _topIds and _itemFetcher
+// todo: create a BehaviorSubject (rxdart) form _itemsOutput
+// todo: create getters to both the streams and the sink
+// todo: create fetchTopIds method
+// todo: create a clearCache method
+// todo: create a transformer that handles the fetching and movement of the item
+// todo: from the constructor link the itemFetcher, transformer, and itemsOutput
+// todo: create a dispose function to close the streams
