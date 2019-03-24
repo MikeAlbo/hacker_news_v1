82078a5028d7468255ded86326e0b71b725e0d55
Michael Albonetti <malbo24@gmail.com>
2019-03-24 11:58:30 -0600

    save changes from before refactoring comments_bloc

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -10,62 +10,11 @@ import '../providers/repository.dart';
 
 class CommentBloc {
   final _repository = Repository();
-  final _singleCommentFetcher = PublishSubject<int>();
-  final _allCommentFetcher = PublishSubject<int>();
+  final _commentsFetcher = PublishSubject<int>();
   final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
-  //constructor
-  CommentBloc() {
-    _allCommentFetcher
-        .transform(_allCommentsTransformer())
-        .pipe(_commentsOutput);
-    _singleCommentFetcher
-        .transform(_singleCommentTransformer())
-        .pipe(_commentsOutput);
-  }
-
-  //Stream
-  Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
-      _commentsOutput.stream;
-
-  //Sink
-  Function(int) get fetchASingleComment => _singleCommentFetcher.sink.add;
-  Function(int) get fetchAllComments => _allCommentFetcher.sink.add;
-
-  //transformers
-
-  //transformer for single comment
-  _singleCommentTransformer() {
-    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
-      (cache, int id, _) {
-        cache[id] = _repository.fetchItem(id);
-        cache[id].then((ItemModel item) {
-          return fetchAllComments(item.kids[0]);
-        });
-        return cache;
-      },
-      <int, Future<ItemModel>>{},
-    );
-  }
-
-  // transformer for all comments
-  _allCommentsTransformer() {
-    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
-      (cache, int id, _) {
-        cache[id] = _repository.fetchItem(id);
-        cache[id].then((ItemModel item) {
-          item.kids.forEach((kidId) => fetchAllComments(kidId));
-        });
-        return cache;
-      },
-      <int, Future<ItemModel>>{},
-    );
-  }
-
-  // cleanup function
   dispose() {
+    _commentsFetcher.close();
     _commentsOutput.close();
-    _singleCommentFetcher.close();
-    _allCommentFetcher.close();
   }
 }
--- lib/src/blocs/comments_provider.dart
+++ lib/src/blocs/comments_provider.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hacker_news_v1/src/blocs/comments_bloc.dart';
 
-import 'comments_bloc.dart';
-
-export 'comments_bloc.dart ';
+export 'comments_bloc.dart';
 
 class CommentsProvider extends InheritedWidget {
   final CommentBloc commentBloc;
